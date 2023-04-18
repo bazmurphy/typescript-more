7b5b1b8a59635f8e7876aff272aa3a5455b83544
bazmurphy <bazmurphy@gmail.com>
2023-04-18 11:21:12 +0100

    adjustments

--- 02-10/app.js
+++ 02-10/app.js
@@ -2,8 +2,10 @@
 function add(n1, n2) {
     return n1 + n2;
 }
-const number1 = "5";
+// const number1 = "5";
+const number1 = 5;
 const number2 = 2.8;
+// Argument of type 'string' is not assignable to parameter of type 'number'
 const result = add(number1, number2);
 const paragraph = document.createElement("paragraph");
 paragraph.innerText = `the result is ${result} \n the type is ${typeof result}`;
--- 02-10/app.ts
+++ 02-10/app.ts
@@ -1,10 +1,12 @@
-function add(n1, n2) {
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-const number1 = "5";
+// const number1 = "5";
+const number1 = 5;
 const number2 = 2.8;
 
+// Argument of type 'string' is not assignable to parameter of type 'number'
 const result = add(number1, number2);
 
 const paragraph = document.createElement("paragraph");
