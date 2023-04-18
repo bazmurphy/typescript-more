17bce415ccb63d0f6eb25c6e299cb3f3a38540c2
bazmurphy <bazmurphy@gmail.com>
2023-04-18 11:35:55 +0100

    adjustments

--- 02-11/app.js
+++ 02-11/app.js
@@ -7,6 +7,5 @@ const number1 = 5;
 const number2 = 2.8;
 // Argument of type 'string' is not assignable to parameter of type 'number'
 const result = add(number1, number2);
-const paragraph = document.createElement("paragraph");
-paragraph.innerText = `the result is ${result} \n the type is ${typeof result}`;
-document.body.appendChild(paragraph);
+console.log(result);
+console.log(typeof result);
