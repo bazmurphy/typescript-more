cdccf633c86f302e146fbb2f070454df82956427
bazmurphy <bazmurphy@gmail.com>
2023-04-18 11:26:40 +0100

    adjustments

--- 02-12/app.js
+++ 02-12/app.js
@@ -1,13 +1,12 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+"use strict";
+function add(n1, n2) {
+    console.log(typeof number1);
+    return n1 + n2;
 }
-
 // const number1 = "5";
 const number1 = 5;
 const number2 = 2.8;
-
 // Argument of type 'string' is not assignable to parameter of type 'number'
 const result = add(number1, number2);
-
 console.log(result);
 console.log(typeof result);
